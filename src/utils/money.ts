e8a3243d40d18e81609b17b8ae7c45d69f5db67f
Lucas Brasil <lucasbrasil1@hotmail.com>
2023-01-31 00:44:00 -0300

    Variable format type for money

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,9 +1,9 @@
 import Dinero from "dinero.js"
 
-export const money = (value : Number | String) => {
+export const money = (value : number | string) => {
     return Dinero({amount : Number(value)})
 }
 
-export const formatMoney = (productPrice: Number) => {
+export const formatMoney = (productPrice: number) => {
     return Dinero({ amount : Number(productPrice) }).toFormat("$0.00")
 }
